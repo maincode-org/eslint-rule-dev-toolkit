24c4a93e87d6575b64e2da67b66a902a67b4f734
Markido <markido@live.dk>
2021-11-25 12:50:30 +0100

    Initial commit

---
+++ tests/trace-value/target-files/file-4.js
@@ -0,0 +1,14 @@
+/**
+ * This file contains values at test-depth "4".
+ * This is the bottom level, and the values here may not reference other project files.
+ */
+
+const a = 'A definite string';
+
+let b = 'A definite string';
+
+var c = 'A definite string';
+
+b = 'Still a definite string';
+
+c = fetch('no-longer-a-definite-string!');
